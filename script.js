7b42156f0350007951fc88121c126791620eb573
Misael Ratiarinambinina <m.ratiarinambinina@e-hoi.de>
2020-12-04 12:48:20 +0100

    premier

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function($) {
+
+    $('#btnFacebook').click(function() {
+      window.location.href = "https://www.facebook.com" ;
+    })
+
+    $('#btnYoutube').click(function() {
+      window.location.href = "https://www.youtube.com" ;
+    })
+
+    $('#btnInstagram').click(function() {
+      window.location.href = "https://www.instagram.com" ;
+    })
+
+    $('#btnTwitter').click(function() {
+      window.location.href = "www.twitter.com" ;
+    })
+
+
+
+});
